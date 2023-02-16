00f1ab91844a5496b072237be306e1fd1c4dd15b
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-16 20:56:15 +0200

    Create Contacts.styled.js

---
+++ src/components/Contacts/Contacts.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+`;
+export const Item = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+  width: 100%;
+  gap: 20px;
+`;
+
+export const Text = styled.p`
+  font-size: 18px;
+`;
