2f79f05d74addaa0fa7afa8df31d66132cba3e50
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-16 20:56:10 +0200

    Create App.styled.js

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 40px auto;
+  padding: 30px 20px;
+
+  min-width: 400px;
+  max-width: 550px;
+  border-radius: 10px;
+  overflow: hidden;
+
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  border: 2px solid #000000;
+`;
+
+export const Title = styled.h2`
+  padding-bottom: 20px;
+  font-weight: 600;
+  font-size: 38px;
+  letter-spacing: 1.2px;
+  /* font-style: italic; */
+  color: #ff6b08;
+  text-shadow: 6px 6px 6px #000;
+  text-align: center;
+`;
