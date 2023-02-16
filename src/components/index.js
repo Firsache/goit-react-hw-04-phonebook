fc0642053aa9e5c3b59abb82e8f77634aaef8005
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-16 20:56:26 +0200

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from 'components/Contacts/Contacts.jsx';
+export * from 'components/Form/Form.jsx';
+export * from 'components/Filter/Filter';
+export * from 'components/Section/Section';
+export * from 'components/Notification/Notification';
