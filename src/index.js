d218a90b5152be0e61a68b74f2bf8aa0de1f4945
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-19 19:52:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
+import { ThemeProvider } from 'styled-components';
+// import {theme} from './styles/theme.jsx'
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
