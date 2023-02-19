5121f1b65adb93c08f4d143e3e177c43aa6b9598
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-19 20:16:46 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export * from 'components/Form/Form.jsx';
 export * from 'components/Filter/Filter';
 export * from 'components/Section/Section';
 export * from 'components/Notification/Notification';
-export * from 'components/ThemeSwitcher/ThemeSwitcher';
+export * from 'components/ThemeSwitcher/ThemeSwitcher.jsx';
 export * from 'components/Box/Box.jsx';
