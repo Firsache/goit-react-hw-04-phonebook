c191992aae09d20c4c07493c00e91f971e526934
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-19 20:16:48 +0200

    Delete ThemeSwitcher.js

--- src/components/ThemeSwitcher/ThemeSwitcher.js
+++
@@ -1,9 +0,0 @@
-import { BsSun, BsMoon } from 'react-icons/bs';
-
-export function ThemeSwitcher(switchTheme, themeTitle) {
-  return (
-    <button type="button" onClick={switchTheme}>
-      {themeTitle === 'light' ? <BsSun size={30} /> : <BsMoon size={30} />}
-    </button>
-  );
-}
