443997698b3f20664621813fa295ae148ae5c0b1
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-20 21:14:11 +0200

    Update Contacts.styled.js

--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -5,16 +5,16 @@ export const List = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 20px;
+  gap: ${p => p.theme.space[4]}px;
 `;
 export const Item = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
   width: 100%;
-  gap: 20px;
+  gap: ${p => p.theme.space[4]}px;
 `;
 
 export const Text = styled.p`
-  font-size: 18px;
+  font-size: ${p => p.theme.fontSizes.m};
 `;
