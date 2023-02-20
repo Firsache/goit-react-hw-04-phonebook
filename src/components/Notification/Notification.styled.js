4a098e7a806cf0cd5c75873af9c035fee0294e78
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-20 21:14:23 +0200

    Update Notification.styled.js

--- src/components/Notification/Notification.styled.js
+++ src/components/Notification/Notification.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Message = styled.p`
-  padding-top: 10px;
-  font-size: 20px;
+  padding-top: ${p => p.theme.space[3]}px;
+  font-size: ${p => p.theme.fontSizes.m};
   font-style: italic;
 `;
