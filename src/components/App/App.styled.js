d8e0dae64974669680db011ec4edde45cf7e513b
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-20 21:14:09 +0200

    Update App.styled.js

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -1,26 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin: 0 auto;
-  padding: 30px 20px;
+  margin: ${p => p.theme.space[5]}px auto;
+  padding: ${p => p.theme.space[5]}px ${p => p.theme.space[4]}px;
 
   min-width: 400px;
   max-width: 550px;
-  /* border-radius: 10px; */
   overflow: hidden;
-
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-  /* border: 2px solid #000000; */
+  box-shadow: 6px 6px 4px 0px ${p => p.theme.colors.shadow},
+    -4px -4px 4px ${p => p.theme.colors.shadow}; ;
 `;
 
 export const Title = styled.h2`
-  font-weight: 600;
-  font-size: 38px;
-  letter-spacing: 1.2px;
+  margin-left: ${p => p.theme.space[3]}px;
   font-size: ${p => p.theme.fontSizes.l};
   font-weight: ${p => p.theme.fontWeights.bold};
   line-height: ${p => p.theme.lineHeights.heading};
-  /* color: #ff6b08;
-  text-shadow: 6px 6px 6px #000;
-  text-align: center; */
+  /* color: ${p => p.theme.colors.white}; */
+  text-shadow: 6px 6px 6px ${p => p.theme.colors.black};
+  text-align: center;
 `;
