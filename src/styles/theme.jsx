a8db659454cd056dc73326d32992e1f37c6c25d9
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-20 16:43:30 +0200

    Update theme.jsx

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,14 +1,4 @@
 export const theme = {
-  //   colors: {
-  //     black: '#000',
-  //     white: '#fff',
-  //     text: '#2a2a2a',
-  //     background: '#fff',
-  //     primary: '#07c',
-  //     secondary: '#05a',
-  //     accent: '#609',
-  //     muted: '#f6f6f6',
-  //   },
   space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
   fontSizes: {
     xs: '12px',
@@ -35,4 +25,5 @@ export const theme = {
     normal: '4px',
     round: '50%',
   },
+  transition: '200ms ease-in-out',
 };
