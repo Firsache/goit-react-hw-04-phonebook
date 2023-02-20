dff2bf5eb67532e48aaf0a904755af23b6974c89
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-20 21:14:06 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,10 @@ import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import { useLocalStorage } from 'hooks/useLocalStorage';
 
+import { MdContactPhone } from 'react-icons/md';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Container, Title } from './App.styled';
 import { GlobalStyles } from 'styles/GlobalStyles/globalStyles.styled';
 import { colors } from 'styles/colors';
@@ -19,8 +23,8 @@ import {
 
 export function App() {
   const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [themeTitle, setThemeTitle] = useLocalStorage('theme', 'light');
   const [filteredName, setFilteredName] = useState('');
-  const [themeTitle, setThemeTitle] = useState('light');
 
   const switchTheme = () => {
     setThemeTitle(prevState => (prevState === 'light' ? 'dark' : 'light'));
@@ -28,11 +32,15 @@ export function App() {
 
   const addContact = ({ name, number }) => {
     if (contacts.some(c => c.name === name)) {
-      alert(`Contact ${name} already exists!`);
+      toast.error(`Contact ${name} already exists!`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
       return;
     }
     if (contacts.some(c => c.number === number)) {
-      alert(`Contact ${number} already exists!`);
+      toast.error(`Contact ${number} already exists!`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
       return;
     }
     setContacts(prevState => [{ id: nanoid(4), name, number }, ...prevState]);
@@ -57,8 +65,23 @@ export function App() {
   return (
     <Layout theme={normalizedTheme}>
       <Box as="section">
-        <Box position="relative" py={4} as="div">
-          <Title>Phonebook</Title>
+        <Box
+          position="relative"
+          py={4}
+          bg={normalizedTheme.colors.accent}
+          as="div"
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            color={normalizedTheme.colors.white}
+            as="div"
+          >
+            <MdContactPhone size={35} />
+            <Title>Phonebook</Title>
+          </Box>
+
           <ThemeSwitcher switchTheme={switchTheme} themeTitle={themeTitle} />
         </Box>
         <Container>
@@ -79,6 +102,7 @@ export function App() {
               <Notification message="There are no contacts in the phonebook yet..." />
             )}
           </Section>
+          <ToastContainer autoClose={3000} />
           <GlobalStyles />
         </Container>
       </Box>
